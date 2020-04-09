2e37f752d00b3993eae3718a455dc881b31bc6b1
JoseInacioDEV <joseinnacioSoft@hotmail.com>
2020-04-09 11:27:21 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/softarquitetura/softarquitetura/SoftArquiteturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.softarquitetura.softarquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftArquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftArquiteturaApplication.class, args);
+	}
+
+}
